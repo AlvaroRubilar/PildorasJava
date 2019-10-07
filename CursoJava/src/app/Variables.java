d89cb5f0f79ca0352e3cc84f08efe5d909225939
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-07 00:30:15 -0300

    video6 variables

---
+++ CursoJava/src/app/Variables.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Variables
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+        byte edad;
+        edad = 35;
+        System.out.println(edad);
+    }
+}
