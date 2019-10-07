7d2257282fca861a810beff31ebad7ce32bb1c90
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-07 00:37:07 -0300

    video 6 imprimir cambios Variables

--- CursoJava/src/app/Variables.java
+++ CursoJava/src/app/Variables.java
@@ -7,6 +7,7 @@ public class Variables {
 
     public static void main(String[] args) {
         int edad = 35;
+        System.out.println(edad);
         edad = 75;
         System.out.println(edad);
     }
