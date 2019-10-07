dfd8b72f7f886a2d45b5a00f3e4d4ea06df2e4f9
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-07 01:09:00 -0300

    video7 operadores

---
+++ CursoJava/src/app/DeclaracionesOperadores.java
@@ -0,0 +1,27 @@
+package app;
+
+/**
+ * DeclaracionesOperadores
+ */
+public class DeclaracionesOperadores {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        int b;
+
+        b = 7;
+
+        int c = b + a;
+
+        c++;
+
+        c+=8;
+
+        System.out.println(c);
+
+
+
+    }
+}
