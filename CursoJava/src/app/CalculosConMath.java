6ceb3c27e2ac02a6ded758371fcd3d6d7fd86c81
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-08 01:37:25 -0300

    video 9 Clase Math

---
+++ CursoJava/src/app/CalculosConMath.java
@@ -0,0 +1,12 @@
+package app;
+
+/**
+ * CalculosConMath
+ */
+public class CalculosConMath {
+
+    public static void main(String[] args) {
+    double raiz=Math.sqrt(9.0);
+    System.out.println(raiz);
+    }
+}
