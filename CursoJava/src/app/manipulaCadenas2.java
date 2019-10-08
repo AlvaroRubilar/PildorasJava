b58e0f4e9ff8ef20a1f45d63e6270f622dd701de
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-08 11:53:47 -0300

    Video 12 equals

--- CursoJava/src/app/manipulaCadenas2.java
+++ CursoJava/src/app/manipulaCadenas2.java
@@ -6,8 +6,9 @@ package app;
 public class manipulaCadenas2 {
 
     public static void main(String[] args) {
-        String frase = "Hoy es un estupendo día para aprender a programar en Java";
-        String fraseResumen = frase.substring(0, 28) + " irnos a la playa y olvidarnos de todo...."+" y "+frase.substring(29,57);
-        System.out.println(fraseResumen);
+        String alumno1, alumno2;
+        alumno1 = "David";
+        alumno2 = "David";
+        System.out.println(alumno1.equals(alumno2));
     }
 }
