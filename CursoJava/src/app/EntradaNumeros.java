c1e3d93ee68e5f419e3b866d3c7d8e3ad7036be8
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-09 10:56:20 -0300

    Video 15 impresión con formato

---
+++ CursoJava/src/app/EntradaNumeros.java
@@ -0,0 +1,12 @@
+package app;
+
+/**
+ * EntradaNumeros
+ */
+public class EntradaNumeros {
+
+    public static void main(String[] args) {
+        double x=10000.0;
+        System.out.printf("%1.2f",x/3);
+    }
+}
