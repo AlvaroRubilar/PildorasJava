1146d3d068089c62899ab768246a0254045dd3f5
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-09 12:37:40 -0300

    Video 19 while adivina número

---
+++ CursoJava/src/app/AdivinaNumero.java
@@ -0,0 +1,29 @@
+package app;
+
+import java.util.Scanner;
+
+/**
+ * AdivinaNumero
+ */
+public class AdivinaNumero {
+
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+        int aleatorio = (int)Math.round(Math.random()*100);
+        int numero=0, intentos=0;
+        while(numero!=aleatorio){
+            intentos++;
+            System.out.println("Introduce un número, por favor");
+            numero = entrada.nextInt();
+            if(numero>aleatorio){
+                System.out.println("Más bajo");
+            }else if (numero<aleatorio) {
+                System.out.println("Más alto");
+                
+            } 
+            
+        }
+        System.out.println("Correcto el número es "+ aleatorio + " en "+intentos+" intentos.");
+        entrada.close();
+    }
+}
