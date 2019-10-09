5351d6b176a27d0523ab035d74d5b55c1a629717
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-09 11:14:31 -0300

    Video 16 condicional if else

--- CursoJava/src/app/EvaluaEdad.java
+++ CursoJava/src/app/EvaluaEdad.java
@@ -8,15 +8,25 @@ import java.util.Scanner;
 public class EvaluaEdad {
 
     public static void main(String[] args) {
-        Scanner entrada= new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Introduce tu edad, por favor:");
-        int edad= entrada.nextInt();
-        if (edad>=18) {
-          System.out.println("Eres mayor de edad");  
+        int edad = entrada.nextInt();
+        if (edad < 18) {
+            System.out.println("Eres un adolescente");
+
+        } else if (edad < 40) {
+
+            System.out.println("Eres Joven");
+
+        } else if (edad < 65) {
+
+            System.out.println("Eres maduro");
+
         } else {
-          System.out.println("Eres menor de edad");
+            
+            System.out.println("Cuídate");
         }
-        
+
         entrada.close();
     }
 }
