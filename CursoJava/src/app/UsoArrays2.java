0287f602cc02faae6f457c3d397a569961d28e49
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-12 20:05:27 -0300

    Video 23 for each aplicado matriz simplificada

--- CursoJava/src/app/UsoArrays2.java
+++ CursoJava/src/app/UsoArrays2.java
@@ -6,19 +6,24 @@ package app;
 public class UsoArrays2 {
 
     public static void main(String[] args) {
-        String paises[] = new String[8];
+        String paises[] = {"España","México","Colombia","Perú","Chile","Argentina","Ecuador","Venezuela"};
         
-        paises[0] = "España";
-        paises[1] = "México";
-        paises[2] = "Colombia";
-        paises[3] = "Perú";
-        paises[4] = "Chile";
-        paises[5] = "Argentina";
-        paises[6] = "Ecuador";
-        paises[7] = "Venezuela";
+        // paises[0] = "España";
+        // paises[1] = "México";
+        // paises[2] = "Colombia";
+        // paises[3] = "Perú";
+        // paises[4] = "Chile";
+        // paises[5] = "Argentina";
+        // paises[6] = "Ecuador";
+        // paises[7] = "Venezuela";
 
-        for (int i = 0; i < paises.length; i++) {
-            System.out.println("País "+ (i+1)+" "+paises[i]);
+
+        // for (int i = 0; i < paises.length; i++) {
+        //     System.out.println("País "+ (i+1)+" "+paises[i]);
+        // }
+        for (String elemento : paises) {
+            System.out.println("País "+elemento);
+            
         }
     }
 }
