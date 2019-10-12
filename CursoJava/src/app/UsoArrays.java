8e9e5c9f52f6f58eb1362859b5892338e19b6b69
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-12 19:40:35 -0300

    Video 23 arrays

---
+++ CursoJava/src/app/UsoArrays.java
@@ -0,0 +1,22 @@
+package app;
+
+/**
+ * UsoArrays
+ */
+public class UsoArrays {
+
+    public static void main(String[] args) {
+        int miMatriz[] = new int[5];
+        miMatriz[0] = 5;
+        miMatriz[1] = 38;
+        miMatriz[2] = -15;
+        miMatriz[3] = 92;
+        miMatriz[4] = 71;
+        
+        System.out.println(miMatriz[0]);
+        System.out.println(miMatriz[1]);
+        System.out.println(miMatriz[2]);
+        System.out.println(miMatriz[3]);
+        System.out.println(miMatriz[4]);
+    }
+}
