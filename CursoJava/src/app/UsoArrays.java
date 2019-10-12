2118520e2c6daf09e1f5a8b4ea3deec2b5e26df3
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-12 19:50:26 -0300

    video 23 declaración de matriz directa

--- CursoJava/src/app/UsoArrays.java
+++ CursoJava/src/app/UsoArrays.java
@@ -6,12 +6,14 @@ package app;
 public class UsoArrays {
 
     public static void main(String[] args) {
-        int miMatriz[] = new int[5];
-        miMatriz[0] = 5;
-        miMatriz[1] = 38;
-        miMatriz[2] = -15;
-        miMatriz[3] = 92;
-        miMatriz[4] = 71;
+        // int miMatriz[] = new int[5];
+
+        // miMatriz[0] = 5;
+        // miMatriz[1] = 38;
+        // miMatriz[2] = -15;
+        // miMatriz[3] = 92;
+        // miMatriz[4] = 71;
+        int miMatriz[] = {5,38,-15,92,71,95,85,65,25,14,78,85,65,45,78,78,95,23};
 
         for (int i = 0; i < miMatriz.length; i++) {
             System.out.println("El valor de índice " + i + " = " + miMatriz[i]);
