d902c9622149d920ca3601ef1f73be0180ce9154
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-12 20:14:40 -0300

    video 24 matriz de numeros aleatorios

--- CursoJava/src/app/UsoArrays2.java
+++ CursoJava/src/app/UsoArrays2.java
@@ -1,6 +1,6 @@
 package app;
 
-import javax.swing.JOptionPane;
+
 
 /**
  * UsoArrays2
@@ -8,16 +8,12 @@ import javax.swing.JOptionPane;
 public class UsoArrays2 {
 
     public static void main(String[] args) {
-        String paises[] = new String[8];
-
-        for (int i = 0; i < paises.length; i++) {
-            paises[i]=JOptionPane.showInputDialog("Introduce País " +(i+1));
-            
+        int matrizAleatorios[]= new int[150];
+        for (int i = 0; i < matrizAleatorios.length; i++) {
+            matrizAleatorios[i]=(int)Math.round(Math.random()*100);
         }
-        
-       
-        for (String elemento : paises) {
-            System.out.println("País "+elemento);
+        for (int numeros : matrizAleatorios) {
+            System.out.print(numeros+ " ");
             
         }
     }
