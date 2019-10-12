0130b824914dcda022f5c7c6023ae8d62f4fb253
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-12 16:54:47 -0300

    video 22 for email arroba y punto

--- CursoJava/src/app/CompruebaMail.java
+++ CursoJava/src/app/CompruebaMail.java
@@ -8,18 +8,22 @@ import javax.swing.JOptionPane;
 public class CompruebaMail {
 
     public static void main(String[] args) {
-        boolean arroba = false;
+        int arroba = 0;
+        boolean punto = false;
 
         String mail = JOptionPane.showInputDialog("Introduce mail");
 
         for (int i = 0; i < mail.length(); i++) {
             if (mail.charAt(i) == '@') {
-                arroba = true;
+                arroba++;
 
             }
+            if (mail.charAt(i) == '.') {
+                punto = true;
 
+            }
         }
-        if (arroba == true) {
+        if (arroba == 1 && punto == true) {
             System.out.println("Es correcto");
         } else {
             System.out.println("No es correcto");
