3bf0a955fd4461f6720394b96c5f4dde2ca45a2e
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-12 20:10:01 -0300

    Video 23 ingreso JOptionPane

--- CursoJava/src/app/UsoArrays2.java
+++ CursoJava/src/app/UsoArrays2.java
@@ -1,26 +1,21 @@
 package app;
 
+import javax.swing.JOptionPane;
+
 /**
  * UsoArrays2
  */
 public class UsoArrays2 {
 
     public static void main(String[] args) {
-        String paises[] = {"España","México","Colombia","Perú","Chile","Argentina","Ecuador","Venezuela"};
+        String paises[] = new String[8];
+
+        for (int i = 0; i < paises.length; i++) {
+            paises[i]=JOptionPane.showInputDialog("Introduce País " +(i+1));
+            
+        }
         
-        // paises[0] = "España";
-        // paises[1] = "México";
-        // paises[2] = "Colombia";
-        // paises[3] = "Perú";
-        // paises[4] = "Chile";
-        // paises[5] = "Argentina";
-        // paises[6] = "Ecuador";
-        // paises[7] = "Venezuela";
-
-
-        // for (int i = 0; i < paises.length; i++) {
-        //     System.out.println("País "+ (i+1)+" "+paises[i]);
-        // }
+       
         for (String elemento : paises) {
             System.out.println("País "+elemento);
             
