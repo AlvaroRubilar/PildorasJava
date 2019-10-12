751c89b21bdd8690040a12bd202941951a823430
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-12 19:51:12 -0300

    video 23 correcciones

--- CursoJava/src/app/UsoArrays.java
+++ CursoJava/src/app/UsoArrays.java
@@ -13,7 +13,7 @@ public class UsoArrays {
         // miMatriz[2] = -15;
         // miMatriz[3] = 92;
         // miMatriz[4] = 71;
-        int miMatriz[] = {5,38,-15,92,71,95,85,65,25,14,78,85,65,45,78,78,95,23};
+        int miMatriz[] = { 5, 38, -15, 92, 71, 95, 85, 65, 25, 14, 78, 85, 65, 45, 78, 78, 95, 23 };
 
         for (int i = 0; i < miMatriz.length; i++) {
             System.out.println("El valor de índice " + i + " = " + miMatriz[i]);
