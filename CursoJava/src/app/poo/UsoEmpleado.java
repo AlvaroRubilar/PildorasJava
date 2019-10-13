2a3e4f1d3070f0ba5752ab119e2ccaf3bd7bef57
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-13 15:36:28 -0300

    video 35 instanciar la clase

--- CursoJava/src/app/poo/UsoEmpleado.java
+++ CursoJava/src/app/poo/UsoEmpleado.java
@@ -1,6 +1,5 @@
 package app.poo;
 
-
 import java.util.*;
 
 /**
@@ -9,7 +8,19 @@ import java.util.*;
 public class UsoEmpleado {
 
     public static void main(String[] args) {
+
+        Empleado empleado1 = new Empleado("Paco Gómez", 85000, 1990, 12, 17);
+        Empleado empleado2 = new Empleado("Ana Lopez", 95000, 1995, 06, 02);
+        Empleado empleado3 = new Empleado("María Martín", 10500, 2002, 03, 15);
         
+        empleado1.subeSueldo(5);
+        empleado2.subeSueldo(5);
+        empleado3.subeSueldo(5);
+
+        System.out.println("Nombre: "+ empleado1.dameNombre()+"\nSueldo: "+empleado1.dameSueldo()+"\nFecha de Alta: "+empleado1.dameFechaContrato());
+        System.out.println("Nombre: "+ empleado2.dameNombre()+"\nSueldo: "+empleado2.dameSueldo()+"\nFecha de Alta: "+empleado2.dameFechaContrato());
+        System.out.println("Nombre: "+ empleado3.dameNombre()+"\nSueldo: "+empleado3.dameSueldo()+"\nFecha de Alta: "+empleado3.dameFechaContrato());
+
     }
 }
 
@@ -19,40 +30,41 @@ public class UsoEmpleado {
 class Empleado {
 
     public Empleado(String nom, double sue, int agno, int mes, int dia) {
-        //cosntructor
-        nombre=nom;
-        
-        sueldo=sue;
+        // cosntructor
+        nombre = nom;
 
-        GregorianCalendar calendario = new GregorianCalendar(agno, mes-1,dia);
-        altaContrato=calendario.getTime();
+        sueldo = sue;
 
+        GregorianCalendar calendario = new GregorianCalendar(agno, mes - 1, dia);
+        altaContrato = calendario.getTime();
 
-        
     }
 
-    public String dameNombre() {//getter
-        
+    public String dameNombre() {// getter
+
         return nombre;
-        
+
     }
-    public double dameSueldo(){//getter
-        
+
+    public double dameSueldo() {// getter
+
         return sueldo;
     }
-    public Date dameFechaContrato(){//getter
-       
+
+    public Date dameFechaContrato() {// getter
+
         return altaContrato;
     }
-    public void subeSueldo(double porcentaje){//setter
-       
-        double aumento= sueldo*porcentaje/100;
-        sueldo+=aumento;
+
+    public void subeSueldo(double porcentaje) {// setter
+
+        double aumento = sueldo * porcentaje / 100;
+        sueldo += aumento;
 
     }
+
     private String nombre;
     private double sueldo;
     private Date altaContrato;
 
-    
 }
