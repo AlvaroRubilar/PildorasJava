87eea698974fed76f7b9d71a6e046d6ef3d57343
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-13 10:55:19 -0300

    video 28 poo

---
+++ CursoJava/src/app/poo/Coche.java
@@ -0,0 +1,21 @@
+package app.poo;
+
+/**
+ * Coche
+ */
+public class Coche {
+
+    int ruedas;
+    int largo;
+    int ancho;
+    int motor;
+    int peso;
+    // método constructor
+    public Coche() {
+        ruedas = 4;
+        largo = 2000;
+        ancho = 300;
+        motor = 1600;
+        peso = 500;
+    }
+}
---
+++ CursoJava/src/app/poo/UsoCoche.java
@@ -0,0 +1,15 @@
+package app.poo;
+
+/**
+ * UsoCoche
+ */
+public class UsoCoche {
+
+    public static void main(String[] args) {
+       
+        Coche Renault = new Coche();// Instanciar una clase. Ejemplar de clase
+       
+        System.out.println("Este coche tiene " + Renault.ruedas + " ruedas");
+        
+    }
+}
