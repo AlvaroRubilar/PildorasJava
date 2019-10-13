cded1de8e45f0ab80176ecb63c0965b312b857b9
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-13 18:52:57 -0300

    video 38 cambio de formato

--- CursoJava/src/app/poo/Pruebas.java
+++ CursoJava/src/app/poo/Pruebas.java
@@ -8,19 +8,17 @@ public class Pruebas {
     public static void main(String[] args) {
 
         Empleados trabajador1 = new Empleados("Paco");
-        
+
         Empleados trabajador2 = new Empleados("Ana");
 
         Empleados trabajador3 = new Empleados("Antonio");
 
         Empleados trabajador4 = new Empleados("María");
-       
+
         trabajador1.cambiaSeccion("RRHH");
 
-        System.out.println(trabajador1.devuelveDatos()+"\n"+trabajador2.devuelveDatos()+"\n"+trabajador3.devuelveDatos()+"\n"+trabajador4.devuelveDatos());
-       
-        
-        
+        System.out.println(trabajador1.devuelveDatos() + "\n" + trabajador2.devuelveDatos() + "\n"
+                + trabajador3.devuelveDatos() + "\n" + trabajador4.devuelveDatos());
 
         System.out.println(Empleados.dameIdsiguiente());
     }
@@ -35,7 +33,7 @@ class Empleados {
 
         nombre = nom;
         seccion = "Administración";
-        Id=IdSiguiente;
+        Id = IdSiguiente;
         IdSiguiente++;
 
     }
@@ -50,13 +48,14 @@ class Empleados {
 
         return "El nombre es " + nombre + ", la sección es " + seccion + " y el Id = " + Id;
     }
-    public static String dameIdsiguiente(){
-        return "El IdSiguiente es "+IdSiguiente;
+
+    public static String dameIdsiguiente() {
+        return "El IdSiguiente es " + IdSiguiente;
     }
 
     private final String nombre;
     private String seccion;
     private int Id;
-    private static int IdSiguiente=1;
+    private static int IdSiguiente = 1;
 
 }
