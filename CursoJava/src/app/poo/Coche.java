4541d668ad9063cb0deeeae0652e7b8d9bae6136
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-13 12:01:48 -0300

    Video 30 setter getter

--- CursoJava/src/app/poo/Coche.java
+++ CursoJava/src/app/poo/Coche.java
@@ -5,17 +5,38 @@ package app.poo;
  */
 public class Coche {
 
-    int ruedas;
-    int largo;
-    int ancho;
-    int motor;
-    int peso;
+    private int largo;
+    private int ancho;
+    private int motor;
+    private int ruedas;
+    private int pesoPlataforma;
+    String color;
+    int pesoTotal;
+    boolean asientosCuero, climatizador;
+
     // método constructor
     public Coche() {
         ruedas = 4;
         largo = 2000;
         ancho = 300;
         motor = 1600;
-        peso = 500;
+        pesoPlataforma = 500;
     }
+
+    public String dimeLargo() {// getter
+
+        return "El largo del coche es " + largo;
+
+    }
+
+    public void estableceColor() {//setter
+        color = "Azul";
+
+    }
+    public String dimeColor() {
+
+        return "El color del coche es "+color;
+        
+    }
+
 }
--- CursoJava/src/app/poo/UsoCoche.java
+++ CursoJava/src/app/poo/UsoCoche.java
@@ -7,11 +7,11 @@ public class UsoCoche {
 
     public static void main(String[] args) {
        
-        Coche Renault = new Coche();// Instanciar una clase. Ejemplar de clase
+        Coche miCoche = new Coche();
+        miCoche.estableceColor();
+        System.out.println(miCoche.dimeColor());
        
-        Renault.ruedas = 3; // Debemos encapsular esta variable
-       
-        System.out.println("Este coche tiene " + Renault.ruedas + " ruedas");
+        
 
     }
 }
