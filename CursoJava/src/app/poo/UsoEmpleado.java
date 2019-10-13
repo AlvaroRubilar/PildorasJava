7c0aee4150a0bafef7b7c382db25ba3198400949
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-13 14:53:47 -0300

    video 34 clase Date, setter y getter

--- CursoJava/src/app/poo/UsoEmpleado.java
+++ CursoJava/src/app/poo/UsoEmpleado.java
@@ -1,6 +1,7 @@
 package app.poo;
 
-import java.util.Date;
+
+import java.util.*;
 
 /**
  * UsoEmpleado
@@ -18,11 +19,37 @@ public class UsoEmpleado {
 class Empleado {
 
     public Empleado(String nom, double sue, int agno, int mes, int dia) {
+        //cosntructor
+        nombre=nom;
+        
+        sueldo=sue;
+
+        GregorianCalendar calendario = new GregorianCalendar(agno, mes-1,dia);
+        altaContrato=calendario.getTime();
 
 
         
     }
 
+    public String dameNombre() {//getter
+        
+        return nombre;
+        
+    }
+    public double dameSueldo(){//getter
+        
+        return sueldo;
+    }
+    public Date dameFechaContrato(){//getter
+       
+        return altaContrato;
+    }
+    public void subeSueldo(double porcentaje){//setter
+       
+        double aumento= sueldo*porcentaje/100;
+        sueldo+=aumento;
+
+    }
     private String nombre;
     private double sueldo;
     private Date altaContrato;
