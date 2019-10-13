2c0b50df16be396a5325c7934a20bce01eba5b4d
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-13 14:31:29 -0300

    video 33 clase empleado

---
+++ CursoJava/src/app/poo/UsoEmpleado.java
@@ -0,0 +1,31 @@
+package app.poo;
+
+import java.util.Date;
+
+/**
+ * UsoEmpleado
+ */
+public class UsoEmpleado {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+/**
+ * Empleado
+ */
+class Empleado {
+
+    public Empleado(String nom, double sue, int agno, int mes, int dia) {
+
+
+        
+    }
+
+    private String nombre;
+    private double sueldo;
+    private Date altaContrato;
+
+    
+}
