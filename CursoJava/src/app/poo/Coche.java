dcfc40109c4596ca927ca38dcb06fb1e39ad8d38
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-13 13:18:43 -0300

    Video 32 metodo setter getter no recomendado

--- CursoJava/src/app/poo/Coche.java
+++ CursoJava/src/app/poo/Coche.java
@@ -25,30 +25,55 @@ public class Coche {
 
     public String dimeDatosGenerales() {// getter
 
-        return "La plataforma del vehículo tiene "+ruedas+" ruedas "+". Mide "+largo/1000+" metros con un ancho de "+ ancho +" cm y un peso de plataforma de "+pesoPlataforma+" Kg";
+        return "La plataforma del vehículo tiene " + ruedas + " ruedas " + ". Mide " + largo / 1000
+                + " metros con un ancho de " + ancho + " cm y un peso de plataforma de " + pesoPlataforma + " Kg";
 
     }
 
-    public void estableceColor(String colorCoche) {//setter
+    public void estableceColor(String colorCoche) {// setter
         color = colorCoche;
 
     }
-    
-    public String dimeColor() {//getter
 
-        return "El color del coche es "+color;
-        
-    }
-    public void configuraAsientos(String asientosCuero){//setter
-        this.asientosCuero=asientosCuero.equalsIgnoreCase("si")? true:false;
-        //operador ternario
-        
+    public String dimeColor() {// getter
+
+        return "El color del coche es " + color;
 
     }
-    public String dimeAsientos() {//getter
-        return asientosCuero? "El coche tiene asientos de cuero":"El coche tiene asientos de serie";
-        
+
+    public void configuraAsientos(String asientosCuero) {// setter
+        this.asientosCuero = asientosCuero.equalsIgnoreCase("si") ? true : false;
+        // operador ternario
+
     }
 
+    public String dimeAsientos() {// getter
+
+        return asientosCuero ? "El coche tiene asientos de cuero" : "El coche tiene asientos de serie";
+
+    }
+
+    public void configuraClimatizador(String climatizador) {// setter
+
+        this.climatizador = climatizador.equalsIgnoreCase("si") ? true : false;
+
+    }
+
+    public String dimeClimatizador() {// getter
+
+        return climatizador ? "El coche incorpora climatizador" : "El coche lleva aire acondicionado";
+
+    }
+
+    public String dimePesoCoche() {// setter getter
+        int pesoCarroceria = 500;// variable local
+        pesoTotal = pesoPlataforma + pesoCarroceria + (asientosCuero ? 50 : 0) + (climatizador ? 20 : 0);
+        return "El peso del coche es " + pesoTotal;
+    }
+
+    public int precioCoche() {//getter
+        int precioFinal = 10000 + (asientosCuero ? 2000 : 0) + (climatizador ? 1500 : 0);
+        return precioFinal;
+    }
 
 }
--- CursoJava/src/app/poo/UsoCoche.java
+++ CursoJava/src/app/poo/UsoCoche.java
@@ -1,5 +1,7 @@
 package app.poo;
 
+import javax.swing.JOptionPane;
+
 /**
  * UsoCoche
  */
@@ -8,11 +10,15 @@ public class UsoCoche {
     public static void main(String[] args) {
        
         Coche miCoche = new Coche();
-        miCoche.estableceColor("marrón");
+        miCoche.estableceColor(JOptionPane.showInputDialog("Introduce Color"));
         System.out.println(miCoche.dimeDatosGenerales());
         System.out.println(miCoche.dimeColor());
-        miCoche.configuraAsientos("no");
+        miCoche.configuraAsientos(JOptionPane.showInputDialog("¿ Tiene asientos de cuero?"));
         System.out.println(miCoche.dimeAsientos());
+        miCoche.configuraClimatizador(JOptionPane.showInputDialog("Tiene climatizador"));
+        System.out.println(miCoche.dimeClimatizador());
+        System.out.println(miCoche.dimePesoCoche());
+        System.out.println("El precio final del coche es "+miCoche.precioCoche());
         
 
     }
