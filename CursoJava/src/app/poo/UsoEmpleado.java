3e8f1984c69abb476cb1499180a67105035340cb
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-16 00:37:23 -0300

    Video 45 polimorfismo cambio nombres

--- CursoJava/src/app/poo/UsoEmpleado.java
+++ CursoJava/src/app/poo/UsoEmpleado.java
@@ -8,18 +8,24 @@ import java.util.*;
 public class UsoEmpleado {
 
     public static void main(String[] args) {
+        Jefatura jefeRRHH = new Jefatura("Álvaro", 55000, 2006, 9, 25);
+        jefeRRHH.estableceIncetivo(2570);
 
-        Empleado misEmpleados[] = new Empleado[4];
-        misEmpleados[0] = new Empleado("Paco Gómez", 85000, 1990, 12, 17);
-        misEmpleados[1] = new Empleado("Ana López", 95000, 1995, 06, 02);
-        misEmpleados[2] = new Empleado("María Martín", 105000, 2002, 03, 15);
-        misEmpleados[3] = new Empleado("Antonio Fernández");
+        Empleado misEmpleados[] = new Empleado[6];
+        misEmpleados[0] = new Empleado("Ana", 30000, 2000, 07, 07);
+        misEmpleados[1] = new Empleado("Carlos", 50000, 1995, 06, 15);
+        misEmpleados[2] = new Empleado("Paco", 25000, 2005, 9, 25);
+        misEmpleados[3] = new Empleado("Antonio", 47500, 2009, 11, 9);
+        misEmpleados[4] = jefeRRHH;// polimorfismo en acción. Principio de sustitución
+        misEmpleados[5] = new Jefatura("María", 95000, 1999, 5, 26);
+
         for (Empleado e : misEmpleados) {
 
             e.subeSueldo(5);
 
         }
         for (Empleado e : misEmpleados) {
+            //En dame sueldo se usa segun la clase llamado enlazado dinámico
             System.out.println("Nombre: " + e.dameNombre() + "\nSueldo: " + e.dameSueldo() + "\nFecha de Alta: "
                     + e.dameFechaContrato() + "\n");
         }
@@ -47,7 +53,7 @@ class Empleado {
 
     public Empleado(String nom) {// constructor
 
-        this(nom, 3000, 2000, 01, 01);
+        this(nom, 30000, 2000, 01, 01);
 
     }
 
