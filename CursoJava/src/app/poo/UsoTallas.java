a4be601f6578a6ef7b1d2d6970a4a14927fa423f
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-17 12:19:51 -0300

    video 48 aplicar formato

--- CursoJava/src/app/poo/UsoTallas.java
+++ CursoJava/src/app/poo/UsoTallas.java
@@ -7,30 +7,29 @@ import java.util.Scanner;
  */
 public class UsoTallas {
 
-   
+    enum Talla {
+        MINI("S"), MEDIANO("M"), GRANDE("L"), MUY_GRANDE("XL");
 
-    enum Talla{
-        MINI("S"),MEDIANO("M"),GRANDE("L"),MUY_GRANDE("XL");
-        private Talla(String abreviatura){//enum solo permite private
-            this.abreviatura=abreviatura;
+        private Talla(String abreviatura) {// enum solo permite private
+            this.abreviatura = abreviatura;
         }
-        public String dameAbreviatura(){
+
+        public String dameAbreviatura() {
             return abreviatura;
         }
+
         private String abreviatura;
     }
-    
+
     public static void main(String[] args) {
 
-        Scanner entrada=new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Escribe una talla: MINI, MEDIANO,  GRANDE, MUY_GRANDE");
-        String entradaDatos=entrada.next().toUpperCase();
-        Talla laTalla=Enum.valueOf(Talla.class, entradaDatos);//ingresa al enum
-        System.out.println("Talla = "+laTalla);
-        System.err.println("Abreviatura = "+laTalla.dameAbreviatura());
+        String entradaDatos = entrada.next().toUpperCase();
+        Talla laTalla = Enum.valueOf(Talla.class, entradaDatos);// ingresa al enum
+        System.out.println("Talla = " + laTalla);
+        System.err.println("Abreviatura = " + laTalla.dameAbreviatura());
         entrada.close();
-
-
 
     }
 }
