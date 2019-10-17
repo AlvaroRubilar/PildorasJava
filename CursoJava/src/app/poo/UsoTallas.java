164fd56c052aefa8577a6184863cabbaa3a4a0d8
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-17 12:18:03 -0300

    video 48 enum con sus métodos

--- CursoJava/src/app/poo/UsoTallas.java
+++ CursoJava/src/app/poo/UsoTallas.java
@@ -1,22 +1,36 @@
 package app.poo;
 
+import java.util.Scanner;
+
 /**
  * UsoTallas
  */
 public class UsoTallas {
 
-    enum Talla {
-        MINI, MEDIANO, GRANDE, MUY_GRANDE
-    };
+   
 
+    enum Talla{
+        MINI("S"),MEDIANO("M"),GRANDE("L"),MUY_GRANDE("XL");
+        private Talla(String abreviatura){//enum solo permite private
+            this.abreviatura=abreviatura;
+        }
+        public String dameAbreviatura(){
+            return abreviatura;
+        }
+        private String abreviatura;
+    }
+    
     public static void main(String[] args) {
 
-        Talla s = Talla.MINI;
-        Talla m = Talla.MEDIANO;
-        Talla l = Talla.GRANDE;
-        Talla xl = Talla.MUY_GRANDE;
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Escribe una talla: MINI, MEDIANO,  GRANDE, MUY_GRANDE");
+        String entradaDatos=entrada.next().toUpperCase();
+        Talla laTalla=Enum.valueOf(Talla.class, entradaDatos);//ingresa al enum
+        System.out.println("Talla = "+laTalla);
+        System.err.println("Abreviatura = "+laTalla.dameAbreviatura());
+        entrada.close();
 
-        
+
 
     }
 }
