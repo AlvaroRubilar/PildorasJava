54b8f9b047133bdacb03c999834528916ee548ac
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-17 11:56:31 -0300

    video 48 no tiene sentido ingresar algo que no es talla

---
+++ CursoJava/src/app/poo/UsoTallas.java
@@ -0,0 +1,16 @@
+package app.poo;
+
+/**
+ * UsoTallas
+ */
+public class UsoTallas {
+
+    public static void main(String[] args) {
+        String talla;
+
+        talla="pequeña";
+        talla = "mediana";
+        talla="Grande";
+        talla = "Azul";
+    }
+}
