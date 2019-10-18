dc279c20d7173e61688a4962f39c9b61bf11d0cb
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-18 10:00:20 -0300

    Video 52 Timer

---
+++ CursoJava/src/app/poo/PruebaTemporizador.java
@@ -0,0 +1,35 @@
+package app.poo;
+
+import java.util.*;
+
+import javax.swing.*;
+import javax.swing.Timer;
+import java.awt.event.*;
+
+/**
+ * PruebaTemporizador
+ */
+public class PruebaTemporizador {
+
+    public static void main(String[] args) {
+
+        DameLaHora oyente = new DameLaHora();
+        Timer miTemporizador = new Timer(5000, oyente);
+        miTemporizador.start();
+        JOptionPane.showMessageDialog(null, "Pulsa Aceptar para detener");
+        System.exit(0);
+
+    }
+}
+
+/**
+ * DameLahora
+ */
+class DameLaHora implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+        Date ahora = new Date();
+        System.out.println("Te pongo la hora cada 5 seg: " + ahora);
+    }
+
+}
