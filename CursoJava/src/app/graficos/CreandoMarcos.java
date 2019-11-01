11a37345a2922f1c9f89594aaae4dbaa61756285
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-11-01 10:48:41 -0300

    video 56 setResizable  cambiar tamaño del marco

--- CursoJava/src/app/graficos/CreandoMarcos.java
+++ CursoJava/src/app/graficos/CreandoMarcos.java
@@ -18,5 +18,6 @@ class MiMarco extends JFrame{
         // setSize(500,300);//tamaño
         // setLocation(500,300);//ubicación
         setBounds(500, 300, 250, 250);// x, y , ancho, alto
+        setResizable(true);//puede cambiar el tamaño de la ventana
     }
 }
