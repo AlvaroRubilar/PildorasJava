ccaf3bcdd9c56fe712bb2846d355bd8dab6eccf8
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-10-31 22:18:22 -0300

    video 55 creación primera ventana

---
+++ CursoJava/src/app/graficos/CreandoMarcos.java
@@ -0,0 +1,20 @@
+package app.graficos;
+import javax.swing.*;
+/**
+ * CreandoMarcos
+ */
+public class CreandoMarcos {
+
+    public static void main(String[] args) {
+
+        MiMarco marco1=new MiMarco();
+        marco1.setVisible(true);
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+}
+class MiMarco extends JFrame{
+    public MiMarco(){
+        setSize(500,300);
+    }
+}
