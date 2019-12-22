5403728da51936b41849a770a0dae3b435af9311
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-22 20:57:14 -0300

    video 61 rellenar con color

---
+++ CursoJava/src/app/graficos/TrabajandoColores.java
@@ -0,0 +1,54 @@
+package app.graficos;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+/**
+ * TrabajandoColores
+ */
+public class TrabajandoColores {
+    public static void main(String[] args) {
+        MarcoConColor miMarco = new MarcoConColor();
+        miMarco.setVisible(true);
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+}
+
+class MarcoConColor extends JFrame {
+    public MarcoConColor() {
+
+        setTitle("Prueba de Dibujo");
+
+        setSize(400, 400);
+
+        LaminaConColor miLamina = new LaminaConColor();
+
+        add(miLamina);
+
+    }
+}
+
+class LaminaConColor extends JPanel {
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200, 150);
+        g2.setPaint(Color.RED);
+        g2.fill(rectangulo);
+
+        Ellipse2D elipse=new Ellipse2D.Double();
+        
+        elipse.setFrame(rectangulo);
+        g2.setPaint(Color.BLUE);
+        
+        g2.fill(elipse);
+        
+        
+
+    }
+}
