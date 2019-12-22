b2315061025d717fc38bf898a4583994d700795a
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-21 23:02:19 -0300

    video 58 escribir texto en una lamina

---
+++ CursoJava/src/app/graficos/EscribiendoEnMarco.java
@@ -0,0 +1,36 @@
+package app.graficos;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * EscribiendoEnMarco
+ */
+public class EscribiendoEnMarco {
+
+    public static void main(String[] args) {
+        MarcoConTexto miMarco = new MarcoConTexto();
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+    }
+
+}
+
+class MarcoConTexto extends JFrame {
+    public MarcoConTexto() {
+        setVisible(true);
+        setSize(600, 450);
+        setLocation(400, 200);
+        setTitle("Primer Texto");
+        Lamina miLamina=new Lamina();
+        add(miLamina);
+    }
+}
+class Lamina extends JPanel{
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawString("Estamos aprendiendo Swing",100,100);
+    }
+
+}
