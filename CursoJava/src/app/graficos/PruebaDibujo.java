69147e1215794bb7d50b93363097eef9009b33a4
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-22 01:11:31 -0300

    video 60 dibujo elipse y círculo

--- CursoJava/src/app/graficos/PruebaDibujo.java
+++ CursoJava/src/app/graficos/PruebaDibujo.java
@@ -40,6 +40,17 @@ class LaminaConFiguras extends JPanel {
         Graphics2D g2 = (Graphics2D) g;
         Rectangle2D rectangulo = new Rectangle2D.Double(100, 100, 200, 150);
         g2.draw(rectangulo);
+        Ellipse2D elipse=new Ellipse2D.Double();
+        elipse.setFrame(rectangulo);
+        g2.draw(elipse);
+        g2.draw(new Line2D.Double(100,100,300,250));
+        double centroEnX=rectangulo.getCenterX();
+        double centroEnY=rectangulo.getCenterY();
+        double radio=Math.sqrt(Math.pow(200, 2)+Math.pow(150,2))/2;
+        Ellipse2D circulo = new Ellipse2D.Double();
+        circulo.setFrameFromCenter(centroEnX, centroEnY, centroEnX+radio, centroEnY+radio);
+        g2.draw(circulo);
+        
 
     }
 }
