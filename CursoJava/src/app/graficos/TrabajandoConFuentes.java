a073f2cc366d3d07c92142fa5acc83aff5930703
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-26 13:29:10 -0300

    video 62 ventana con fuente arial

---
+++ CursoJava/src/app/graficos/TrabajandoConFuentes.java
@@ -0,0 +1,35 @@
+package app.graficos;
+import java.awt.*;
+import javax.swing.*;
+/**
+ * TrabajandoConFuentes
+ */
+public class TrabajandoConFuentes {
+
+    public static void main(String[] args) {
+
+        MarcoConFuentes miMarco = new MarcoConFuentes();
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        miMarco.setVisible(true);
+        
+    }
+}
+class MarcoConFuentes extends JFrame{
+    public  MarcoConFuentes() {
+        setTitle("Pruebas con Fuentes");
+        setSize(400,400);
+        LaminaConFuentes miLamina = new LaminaConFuentes();
+        add(miLamina);
+        
+    }
+}
+class LaminaConFuentes extends JPanel{
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2=(Graphics2D) g;
+        Font miFuente = new Font("Arial",Font.BOLD,26);
+        g2.setFont(miFuente);
+        g2.drawString("Alvaro", 100, 100);
+        
+    }
+}
