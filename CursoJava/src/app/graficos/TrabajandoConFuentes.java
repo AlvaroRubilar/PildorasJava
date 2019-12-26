0ac264e6798536b927bf7d8bea67bc655cb53344
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-26 13:31:08 -0300

    video 62 cambio a Courier

--- CursoJava/src/app/graficos/TrabajandoConFuentes.java
+++ CursoJava/src/app/graficos/TrabajandoConFuentes.java
@@ -27,7 +27,7 @@ class LaminaConFuentes extends JPanel{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2=(Graphics2D) g;
-        Font miFuente = new Font("Arial",Font.BOLD,26);
+        Font miFuente = new Font("Courier New",Font.BOLD,26);
         g2.setFont(miFuente);
         g2.drawString("Alvaro", 100, 100);
         
