6684626d78079b08659fc1a2e098ff95e715ad9b
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-26 12:56:41 -0300

    video 62 buscar fuente instalada

--- CursoJava/src/app/graficos/Pruebas.java
+++ CursoJava/src/app/graficos/Pruebas.java
@@ -1,16 +1,20 @@
 package app.graficos;
+
 import java.awt.*;
 import javax.swing.*;
+
 /**
  * Pruebas
  */
 public class Pruebas {
 
     public static void main(String[] args) {
-        
-        String [] nombresDeFuentes=GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
-        for(String nombreDeLaFuente: nombresDeFuentes){
-            System.out.println(nombreDeLaFuente);
+        String fuente = JOptionPane.showInputDialog("Introduce la fuente");
+        boolean estaLaFuente=false;
+        String[] nombresDeFuentes = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        for (String nombreDeLaFuente : nombresDeFuentes) {
+            estaLaFuente = (nombreDeLaFuente.equals(fuente)) ? true :estaLaFuente;
         }
+        System.out.println((estaLaFuente==true)? "Fuente instalada":"No está instalada la fuente");
     }
 }
