fdedc718b1d066cdeab3f76063daaf0d38915a8a
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-27 00:44:45 -0300

    video 64 mosaico con cualquier imagen

--- CursoJava/src/app/graficos/PruebaImagenes.java
+++ CursoJava/src/app/graficos/PruebaImagenes.java
@@ -1,4 +1,5 @@
 package app.graficos;
+
 import java.awt.*;
 import javax.swing.*;
 import javax.imageio.*;
@@ -14,37 +15,41 @@ public class PruebaImagenes {
         MarcoImagen miMarco = new MarcoImagen();
         miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         miMarco.setVisible(true);
-        
+
     }
 }
-class MarcoImagen extends JFrame{
-    public  MarcoImagen() {
+
+class MarcoImagen extends JFrame {
+    public MarcoImagen() {
         setTitle("Marco con Imagen");
         setBounds(500, 300, 300, 200);
         LaminaConImagen miLamina = new LaminaConImagen();
         add(miLamina);
-        
+
     }
 }
-class LaminaConImagen extends JPanel{
+
+class LaminaConImagen extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         // File miImagen= new File("CursoJava/src/app/graficos/retriever.png");
         try {
-            imagen = ImageIO.read(new File("CursoJava/src/app/graficos/xbox.png"));
-            
+            imagen = ImageIO.read(new File("CursoJava/src/app/graficos/retriever.png"));
+
         } catch (IOException e) {
             System.out.println("La imagen no se encuentra");
         }
-        
-        g.drawImage(imagen, 0, 0,null);
+        int anchuraImagen = imagen.getWidth(this);
+        int alturaImagen = imagen.getHeight(this);
+        g.drawImage(imagen, 0, 0, null);
         for (int i = 0; i < 300; i++) {
-            for (int j = 0; j < 200; j++) {
-                
-                g.copyArea(0, 0, 10  , 10, i*10, j*10);
-            }
+        for (int j = 0; j < 200; j++) {
+
+        g.copyArea(0, 0, anchuraImagen , alturaImagen, i*anchuraImagen, j*alturaImagen);
         }
-        
+        }
+
     }
+
     private Image imagen;
 }
