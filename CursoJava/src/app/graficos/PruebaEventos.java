47ee4131275e5d014fc586cc4f2796dec1d65125
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-31 17:29:03 -0300

    video 66 se agregan nuevos botones

--- CursoJava/src/app/graficos/PruebaEventos.java
+++ CursoJava/src/app/graficos/PruebaEventos.java
@@ -1,4 +1,5 @@
 package app.graficos;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -12,28 +13,47 @@ public class PruebaEventos {
         MarcoBotones miMarco = new MarcoBotones();
         miMarco.setVisible(true);
         miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
     }
-   
+
 }
-class MarcoBotones extends JFrame{
+
+class MarcoBotones extends JFrame {
     public MarcoBotones() {
         setTitle("Botones y Eventos");
-        setBounds(700,300,500,300);
+        setBounds(700, 300, 500, 300);
         LaminaBotones miLamina = new LaminaBotones();
         add(miLamina);
 
     }
 }
-class LaminaBotones extends JPanel implements ActionListener{
+
+class LaminaBotones extends JPanel implements ActionListener {
     JButton botonAzul = new JButton("Azul");
+    JButton botonAmarillo = new JButton("Amarillo");
+    JButton botonRojo = new JButton("Rojo");
+
     public LaminaBotones() {
         add(botonAzul);
+        add(botonAmarillo);
+        add(botonRojo);
         botonAzul.addActionListener(this);
+        botonAmarillo.addActionListener(this);
+        botonRojo.addActionListener(this);
     }
-    public void actionPerformed(ActionEvent e) {
 
-        setBackground(Color.BLUE);
+    public void actionPerformed(ActionEvent e) {
+        Object botonPulsado=e.getSource();
+       if (botonPulsado==botonAzul){
+
+           setBackground(Color.BLUE);
+       }else if(botonPulsado==botonRojo){
+           setBackground(Color.RED);
+
+       }
+       else{
+           setBackground(Color.YELLOW);
+       }
     }
 
 }
