48e93cee22c8f4512361db43db4f26b03b830bee
Alvaro Rubilar <alvaro.rubilar@outlook.com>
2019-12-31 13:01:15 -0300

    video 65 creacion marco y lámina

---
+++ CursoJava/src/app/graficos/PruebaEventos.java
@@ -0,0 +1,29 @@
+package app.graficos;
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * PruebaEventos
+ */
+public class PruebaEventos {
+
+    public static void main(String[] args) {
+        MarcoBotones miMarco = new MarcoBotones();
+        miMarco.setVisible(true);
+        miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+    }
+   
+}
+class MarcoBotones extends JFrame{
+    public MarcoBotones() {
+        setTitle("Botones y Eventos");
+        setBounds(700,300,500,300);
+        LaminaBotones miLamina = new LaminaBotones();
+        add(miLamina);
+
+    }
+}
+class LaminaBotones extends JPanel{
+
+}
